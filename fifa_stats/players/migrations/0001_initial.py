69a69f34d1721174c8764ea83a5cfb57cddb36ff
Spike <sfrye@periscope.com>
2013-11-13 16:51:37 -0600

    Adding initial apps and requirements txt file.
    NO TICKET

---
+++ fifa_stats/players/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Player'
+        db.create_table(u'players_player', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+        ))
+        db.send_create_signal(u'players', ['Player'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Player'
+        db.delete_table(u'players_player')
+
+
+    models = {
+        u'players.player': {
+            'Meta': {'object_name': 'Player'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['players']
---
+++ fifa_stats/players/models.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext as _
+from django.db import models
+
+# Create your models here.
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = _('player')
+        verbose_name_plural = _('players')
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
